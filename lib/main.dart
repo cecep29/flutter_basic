2721e1bc3284e655438f8e89cae8cefc0f8ced20
cecep29 <cecepefendi.c2p@gmail.com>
2022-12-29 05:30:00 +0700

    listview add remove

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,61 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> myList = [const Text('Test')];
+
+  int number = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Text Style'),
+          title: const Text('List View'),
         ),
-        body: const Center(
-          child: Text(
-            'Belajar TextStyle',
-            style: TextStyle(
-                fontSize: 30,
-                fontFamily: 'CrashLanding',
-                decoration: TextDecoration.overline,
-                decorationColor: Colors.red,
-                decorationThickness: 5),
-          ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  child: const Text('Add List'),
+                  onPressed: () {
+                    setState(() {
+                      myList.add(
+                        Text(' List ke: $number'),
+                      );
+                      number++;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('Remove List'),
+                  onPressed: () {
+                    setState(() {
+                      myList.removeLast();
+                      number = number - 1;
+                    });
+                  },
+                )
+              ],
+            ),
+            Expanded(
+              child: ListView(
+                children: myList.toList(),
+              ),
+            )
+          ],
         ),
       ),
     );
