94a54c1b8af7318446e20fbae5ab4e88306e25c9
cecep29 <cecepefendi.c2p@gmail.com>
2022-12-29 14:34:49 +0700

    image

--- lib/main.dart
+++ lib/main.dart
@@ -14,115 +14,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Stack dan Align'),
+          title: const Text('Image'),
         ),
-        body: Stack(
-          children: [
-            Column(
-              children: [
-                Flexible(
-                  flex: 1,
-                  child: Row(
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Container(
-                          color: Colors.white,
-                        ),
-                      ),
-                      Flexible(
-                        flex: 1,
-                        child: Container(
-                          color: Colors.black12,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Flexible(
-                  flex: 1,
-                  child: Row(
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Container(
-                          color: Colors.black12,
-                        ),
-                      ),
-                      Flexible(
-                        flex: 1,
-                        child: Container(
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            ListView(
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  child: const Text(
-                    'ini adalah text yang berada di bagian tengah stack',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  child: const Text(
-                    'ini adalah text yang berada di bagian tengah stack',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  child: const Text(
-                    'ini adalah text yang berada di bagian tengah stack',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  child: const Text(
-                    'ini adalah text yang berada di bagian tengah stack',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  child: const Text(
-                    'ini adalah text yang berada di bagian tengah stack',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  child: const Text(
-                    'ini adalah text yang berada di bagian tengah stack',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  child: const Text(
-                    'ini adalah text yang berada di bagian tengah stack',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                )
-              ],
-            ),
-            const Align(
-              alignment: Alignment(0, 0.90),
-              child: ElevatedButton(
-                onPressed: null,
-                style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Colors.red)),
-                child: Text('My Button'),
-              ),
-            )
-          ],
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            //   child: Image.network(
+            //       'https://cdn-asset.jawapos.com/wp-content/uploads/2019/01/resmi-berkencan-kini-jennie-blackpink-miliki-2-kai-kesayangan_m_-750x500.jpg'),
+            // ),
+            child: Image.asset('images/jenni.webp'),
+          ),
         ),
       ),
     );
